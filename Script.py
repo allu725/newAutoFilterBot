9b2ccc55fbb7c34dfe57aef9d19e7069762979a6
allu <62532710+allu725@users.noreply.github.com>
2023-06-06 23:39:41 +0530

    Update Script.py
    H

--- Script.py
+++ Script.py
@@ -1,46 +1,70 @@
 class script(object):
+
     START_TXT = """👋 Hello {},
 
-I can give any books and notes to auto filter method. 🥳
+I can give any book and notes to auto filter method. 🥳
 
 
 
+. 😎"""
 
+    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
 
-    MY_ABOUT_TXT = """★ Server: <a href=https://www.render>render</a>
 ★ Database: <a href=https://www.mongodb.com>MongoDB</a>
+
 ★ Language: <a href=https://www.python.org>Python</a>
+
 ★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
 
-    MY_OWNER_TXT = """★ Name: althaf mk
-★ Username: 
-★ ID: <code>+7373773</code>
-★ Country:  India"""
+    MY_OWNER_TXT = """★ Name: Hansaka Anuhas
+
+★ Username: @altahf
+
+★ ID: <code>5493832202</code>
+
+★ Country: India 🇱🇰"""
 
     STATUS_TXT = """🗂 Total Files: <code>{}</code>
+
 👤 Total Users: <code>{}</code>
+
 👥 Total Chats: <code>{}</code>
+
 ✨ Used Storage: <code>{}</code>
+
 ⚡️ Free Storage: <code>{}</code>"""
 
     NEW_GROUP_TXT = """#NewGroup
+
 ★ Title: {}
+
 ★ ID: <code>{}</code>
+
 ★ Total Members: {}
+
 ★ Added by: {}"""
 
     NEW_USER_TXT = """#NewUser
+
 ★ Name: {}
+
 ★ ID: <code>{}</code>"""
 
     NO_RESULT_TXT = """#NoResult
+
 ★ Group Name: {}
+
 ★ Group ID: <code>{}</code>
+
 ★ Name: {}
 
 ★ Message: {}"""
 
     REQUEST_TXT = """★ Name: {}
+
 ★ ID: <code>{}</code>
 
 ★ Message: {}"""
+
+
+
